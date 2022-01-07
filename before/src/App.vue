db566d3a2d850bc066857d2ee5b4bf19bbb55c1b
liufan <1014229769@qq.com>
2022-01-07 21:53:22 +0800

    添加主界面左侧菜单栏

--- before/src/App.vue
+++ before/src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
+    <!-- 头部 -->
     <Header />
+    <!-- 主体 -->
+    <div class="body">
+      <!-- 侧边栏 -->
+      <Aside class="body_aside" />
+      <!-- 内容区域 -->
+      <router-view />
+    </div>
   </div>
-  <router-view />
 </template>
 
 <script>
-import Header from "@/components/Header.vue"
+import Header from "@/components/Header.vue";
+import Aside from "@/components/Aside.vue";
 
 export default {
   name: "Layout",
   components: {
     Header,
-  }
-}
+    Aside,
+  },
+};
 </script>
 
+<style scoped>
+.body {
+  display: flex;
+}
+.body_aside {
+  width: auto;
+}
+</style>
---
+++ before/src/components/aside.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    @open="handleOpen"
+    @close="handleClose"
+    style="width: 200px;height:90vh"
+  >
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>Navigator One</span>
+      </template>
+      <el-menu-item-group title="Group One">
+        <el-menu-item index="1-1">item 1</el-menu-item>
+        <el-menu-item index="1-2">item 2</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <span>Navigator Two</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon><document /></el-icon>
+      <span>Navigator Three</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon><setting /></el-icon>
+      <span>Navigator Four</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "aside",
+  components: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* .tac_aside {
+  width: 400px;
+  height: 100vh-50px;
+} */
+</style>

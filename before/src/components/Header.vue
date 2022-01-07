6e28807d4970d4efcd1c8c0ee391f30bc590b630
liufan <1014229769@qq.com>
2022-01-07 18:02:09 +0800

    添加主界面顶部导航栏

---
+++ before/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header_left">后台管理</div>
+    <div class="header_middle"></div>
+    <div class="header_right">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          刘凡
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  height: 50px;
+  line-height: 50px;
+  border-bottom: 1px solid #ccc;
+  display: flex;
+}
+.header_left {
+  width: auto;
+  padding-left: 30px;
+  font-weight: bold;
+  color: dodgerblue;
+}
+.header_middle {
+  flex: 1;
+}
+.header_right {
+  width: auto;
+}
+</style>
--- before/src/views/Home.vue
+++ before/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  home
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
+
+<style scoped>
+</style>

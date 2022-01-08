2fc17fe91ede3698e3d943c9490e762370c9bd39
liufan <1014229769@qq.com>
2022-01-08 18:44:38 +0800

    创建Spring Web+Lombok +MyBatis Framework+MySQL Driver结构项目

---
+++ back/src/test/java/com/lf/backmanage/BackmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lf.backmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
